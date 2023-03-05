34d5082471e2d70908fa6e534d44dbb22483f6a9
AntonStrizov <116605290+AntonStrizov@users.noreply.github.com>
2023-03-05 15:50:19 +0200

    Add files via upload

---
+++ Filter/script.js
@@ -0,0 +1,48 @@
+let checkboxAll = document.querySelector(".checkAll");
+let checkbox = document.querySelectorAll(".check");
+let products = document.querySelectorAll(".product");
+function check(clarification){
+    if(clarification == 1){
+        let checknum = 0;
+        for(let y = 0; y < checkbox.length;y++){
+                if(checkbox[y].checked == ""){
+                    checkboxAll.checked = "";
+                    for(let u = 0; u < products.length; u++){
+                        if(checkbox[y].id == products[u].id){
+                            products[u].style.display = "none";
+                        }
+                    }
+                }
+                
+        }
+        for(let y = 0; y < checkbox.length; y++){
+            if(checkbox[y].checked != ""){
+                checknum++;
+                for(let u = 0; u < products.length; u++){
+                    if(checkbox[y].id == products[u].id){
+                        products[u].style.display = "inline-block";
+                    }
+                }
+            }
+            if(checknum == checkbox.length){
+                checkboxAll.checked = "true";
+            }            
+        }
+    }
+    if(checkboxAll.checked != ""){
+        for(let i = 0; i < products.length;i++){
+            products[i].style.display = "inline-block";
+        }
+        for(let y = 0; y < checkbox.length;y++){
+            checkbox[y].checked = "true";
+        }
+     }
+}
+for(let i = 0; i < checkbox.length; i++){
+    checkbox[i].addEventListener("change", function(){
+        check(1);
+    })
+}
+checkboxAll.addEventListener("change", function(){
+    check();
+})
